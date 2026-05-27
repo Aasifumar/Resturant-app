ac7cc0cc1755acb687f832ffcb3cd976223c7986
Nikhil <nikhil.ambadkar@5exception.com>
2026-05-27 18:32:46 +0530

    Fix: Move imports to top of Toast.js to resolve ESLint import order error

--- src/components/common/Toast.js
+++ src/components/common/Toast.js
@@ -1,11 +1,10 @@
 // Toast.js - Toast notification setup
 import { Toaster } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 
 export const Toast = () => <Toaster />;
 
 // Export toast functions
-import toast from 'react-hot-toast';
-
 export const showToast = {
   success: (message) => toast.success(message),
   error: (message) => toast.error(message),
